a5551a7ffb4f0071c907ad08cff515c93ebc5a65
nworrel <nworrel@MGC1P0643W0F.fbpld77.ford.com>
2022-07-15 15:07:43 -0400

    added a messages map to track all messages sent between people

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ app.use(cors());
 
 const server = http.createServer(app); // this is how we create a http server using express
 
+let messages = new Map();
+
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
@@ -18,10 +20,14 @@ io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
     socket.on("send_message", (data) => {
         console.log(data);
+        messages.get(socket.id).push(data.message);
         socket.to(data.room).emit("receive_message", data);
     })
     socket.on("join_room", (data) => {
         socket.join(data);
+        if(!messages.has(socket.id)){
+            messages[socket.id] = [];
+        }
     })
 })
 
